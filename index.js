d580d1faff02f841a0ea7c4b7f8b19b2c043dd06
KoryNunn <knunn187@gmail.com>
2014-03-24 16:28:00 +1000

    No longer turns on for good phones

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function makeIOS7LessShit(){
-    if(!['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) < 0){
+    if(['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) < 0){
         // Yay! your device isnt shit! (or it's a windows phone)
         return;
     }
